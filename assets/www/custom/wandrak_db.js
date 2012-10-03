0b6e6d9ae9a9733ce0c24fb0c529918cc5dc3c47
Stevo <s@example.com>
2012-10-03 16:43:54 +0200

    db - window.localStorage compatibility fix geolocation - fine location fetch fixed (plus timeouts)

--- assets/www/custom/wandrak_db.js
+++ assets/www/custom/wandrak_db.js
@@ -16,41 +16,41 @@ function db_canStoreToServer() {
 }
 
 function db_nextLocalPoiId() {
-	var lastId = window.localStorage.getItem(DB_LOCAL_POI_LAST_ID);
+	var lastId = localStorage.getItem(DB_LOCAL_POI_LAST_ID);
 
 	if (!lastId) {
 		lastId = 1;
-		window.localStorage.setItem(DB_LOCAL_POI_LAST_ID, lastId);
+		localStorage.setItem(DB_LOCAL_POI_LAST_ID, lastId);
 	}
 
 	lastId++;
-	window.localStorage.setItem(DB_LOCAL_POI_LAST_ID, lastId);
+	localStorage.setItem(DB_LOCAL_POI_LAST_ID, lastId);
 
 	return 'local' + lastId;
 }
 
 function db_storeItem(item) {
-	var keysToSend = JSON.parse(window.localStorage.getItem(DB_UNSAVED_ITEM_KEY_LIST));
+	var keysToSend = db_JSONparseCheckNull(localStorage.getItem(DB_UNSAVED_ITEM_KEY_LIST));
 
 	if (keysToSend == null) {
 		keysToSend = {};
-		window.localStorage.setItem(DB_UNSAVED_ITEM_KEY_LIST, JSON.stringify(keysToSend));
+		localStorage.setItem(DB_UNSAVED_ITEM_KEY_LIST, JSON.stringify(keysToSend));
 	}
 
 	var storeItemKey = DB_ITEM_PREFIX_KEY + item.localId;
-	var currentItemValue = window.localStorage.getItem(storeItemKey);
+	var currentItemValue = localStorage.getItem(storeItemKey);
 	if (currentItemValue && currentItemValue.redirect) {
 		storeItemKey = currentItemValue.redirect;
 	}
-	window.localStorage.setItem(storeItemKey, JSON.stringify(item));
+	localStorage.setItem(storeItemKey, JSON.stringify(item));
 	keysToSend[item.localId] = item.localId;
-	window.localStorage.setItem(DB_UNSAVED_ITEM_KEY_LIST, JSON.stringify(keysToSend));
+	localStorage.setItem(DB_UNSAVED_ITEM_KEY_LIST, JSON.stringify(keysToSend));
 
 	return storeItemKey;
 }
 
 function db_readItems() {
-	var keysToSend = JSON.parse(window.localStorage.getItem(DB_UNSAVED_ITEM_KEY_LIST));
+	var keysToSend = db_JSONparseCheckNull(localStorage.getItem(DB_UNSAVED_ITEM_KEY_LIST));
 
 	if (keysToSend == null) {
 		return [];
@@ -67,12 +67,12 @@ function db_readItems() {
 
 function db_readItem(dbKey) {
 	var storeItemKey = DB_ITEM_PREFIX_KEY + dbKey;
-	return JSON.parse(window.localStorage.getItem(storeItemKey));
+	return db_JSONparseCheckNull(localStorage.getItem(storeItemKey));
 }
 
 function db_clearData() {
-	var keysToSend = JSON.parse(window.localStorage.getItem(DB_UNSAVED_ITEM_KEY_LIST));
-	window.localStorage.removeItem(DB_UNSAVED_ITEM_KEY_LIST);
+	var keysToSend = db_JSONparseCheckNull(localStorage.getItem(DB_UNSAVED_ITEM_KEY_LIST));
+	localStorage.removeItem(DB_UNSAVED_ITEM_KEY_LIST);
 
 	if (keysToSend == null) {
 		return;
@@ -81,7 +81,7 @@ function db_clearData() {
 	$.each(keysToSend, function (itemKey, value) {
 		if (itemKey && itemKey.indexOf('local') < 0) {
 			var storeItemKey = DB_ITEM_PREFIX_KEY + itemKey;
-			window.localStorage.removeItem(storeItemKey);
+			localStorage.removeItem(storeItemKey);
 		}
 	});
 }
@@ -110,7 +110,7 @@ function db_sendItemRecc(statusObj) {
 		    		if (item.localId && item.localId.indexOf && item.localId.indexOf('local') >= 0) {
 			    		var storeItemKey = DB_ITEM_PREFIX_KEY + item.localId;
 			    		item.redirect = data.post.id;
-			    		window.localStorage.setItem(storeItemKey, JSON.stringify(item));
+			    		localStorage.setItem(storeItemKey, JSON.stringify(item));
 		    		}
 
 		    		statusObj.pos = statusObj.pos + 1;
@@ -167,13 +167,13 @@ function db_storePages(pagesJson) {
 		postsCleared.push({ id: post.id, title: post.title });
 	});
 
-	window.localStorage.setItem(DB_OFFLINE_PAGE_KEY, JSON.stringify(postsCleared));
+	localStorage.setItem(DB_OFFLINE_PAGE_KEY, JSON.stringify(postsCleared));
 }
 
 function db_loadPages() {
-	var pagesStr = window.localStorage.getItem(DB_OFFLINE_PAGE_KEY);
+	var pagesStr = localStorage.getItem(DB_OFFLINE_PAGE_KEY);
 	if (pagesStr) {
-		return JSON.parse(pagesStr);
+		return db_JSONparseCheckNull(pagesStr);
 	}
 	return null;
 }
@@ -184,13 +184,20 @@ function db_storePois(pageId, poisJson) {
 		poisCleared.push({ id: post.id, title: post.title, content: post.content, localId: post.localId, custom_fields: post.custom_fields });
 	});
 	var pageData = { id: poisJson.id, title: poisJson.title, children: poisCleared };
-	window.localStorage.setItem(DB_OFFLINE_POI_PREFIX_KEY + pageId, JSON.stringify(pageData));
+	localStorage.setItem(DB_OFFLINE_POI_PREFIX_KEY + pageId, JSON.stringify(pageData));
 }
 
 function db_loadPois(pageId) {
-	var poisStr = window.localStorage.getItem(DB_OFFLINE_POI_PREFIX_KEY + pageId);
+	var poisStr = localStorage.getItem(DB_OFFLINE_POI_PREFIX_KEY + pageId);
 	if (poisStr) {
-		return JSON.parse(poisStr);
+		return db_JSONparseCheckNull(poisStr);
 	}
 	return null;
+}
+
+function db_JSONparseCheckNull(str) {
+	if (str == null) {
+		return {};
+	}
+	return JSON.parse(str);
 }
